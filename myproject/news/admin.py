2b4e3834c97fbc0ce6ffbdc8897ec6677f3f8830
jaswa <jaswanthjai1726@gmail.com>
2026-06-30 13:45:05 +0530

    Remove old Django and ML project files, keep only newshub MERN project

--- myproject/news/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import UserPreference
-
-@admin.register(UserPreference)
-class UserPreferenceAdmin(admin.ModelAdmin):
-    list_display = ['user', 'language', 'edition', 'area', 'keywords']
